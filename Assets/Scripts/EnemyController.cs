ae2ba0beed0d7a28123ec98507ce2534448b6d9d
BlackHold2K <cuteza456@gmail.com>
2023-03-07 19:33:30 +0700

    Implement basic ai enemy

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,29 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using System.Linq;
+using System;
 
 [RequireComponent(typeof(Rigidbody))]
 public class EnemyController : MonoBehaviour
 {
+    [SerializeField] float HealthPoint;
     [SerializeField] float speed;
 
+    [SerializeField] float triggerRadius;
+    [SerializeField] LayerMask playerLayer;
+
+
+    float currentHP;
+
     Vector3 direction;
+    [SerializeField] PlayerController playerController;
 
     Rigidbody rigidbody;
+    NavMeshAgent navMeshAgent;
+
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+        currentHP = HealthPoint;
     }
 
     void FixedUpdate()
     {
-        Move();
+        SetAIDistination();
     }
     
-    void Move()
+    void SetAIDistination()
     {
-        
+        IsPlayerInTriggerArea();
+    }
 
-        rigidbody.velocity = direction * speed;
+    bool IsPlayerInTriggerArea()
+    {
+        var overlapCollider = Physics.OverlapSphere(transform.position, triggerRadius,playerLayer);
+
+        foreach(var collider in overlapCollider)
+        {
+            if (collider.TryGetComponent<PlayerController>(out var player))
+            {
+                navMeshAgent.speed = speed;
+                navMeshAgent.SetDestination(playerController.transform.position);
+                return false;
+            }
+        }
+
+        navMeshAgent.speed = 0;
+        return true;
+        
+    }
+
+    void OnDrawGizmos() 
+    {
+        Gizmos.DrawWireSphere(transform.position,triggerRadius);
     }
 }
