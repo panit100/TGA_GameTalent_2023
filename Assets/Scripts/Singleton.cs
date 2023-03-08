ab263a7df5ce2dc1e97e24d442e93762d6c652fa
BlackHold2K <cuteza456@gmail.com>
2023-03-08 11:52:16 +0700

    Add Singleton class

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance {get; protected set;}
+
+    protected virtual void Awake() 
+    {
+        InitAfterAwake();
+    }
+
+    protected abstract void InitAfterAwake();
+
+    protected Singleton()
+    {
+        if(Instance != null && Instance != this)
+            throw new System.Exception($"There are 2 Singleton in Scene. Plase remove one of them.");
+        else
+            Instance = this as T;
+    }
+}
+
+
