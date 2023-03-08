f33eadf34caeff8084df5aa416cfcc0a6e088303
BlackHold2K <cuteza456@gmail.com>
2023-03-08 11:54:56 +0700

    add namespace to singleton class

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -2,23 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+namespace CCB.Core
 {
-    public static T Instance {get; protected set;}
-
-    protected virtual void Awake() 
+    public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     {
-        InitAfterAwake();
-    }
+        public static T Instance {get; protected set;}
 
-    protected abstract void InitAfterAwake();
+        protected virtual void Awake() 
+        {
+            InitAfterAwake();
+        }
 
-    protected Singleton()
-    {
-        if(Instance != null && Instance != this)
-            throw new System.Exception($"There are 2 Singleton in Scene. Plase remove one of them.");
-        else
-            Instance = this as T;
+        protected abstract void InitAfterAwake();
+
+        protected Singleton()
+        {
+            if(Instance != null && Instance != this)
+                throw new System.Exception($"There are 2 Singleton in Scene. Plase remove one of them.");
+            else
+                Instance = this as T;
+        }
     }
 }
 
